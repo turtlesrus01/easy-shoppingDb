f282700ee70c9a2c00c4ed4c2da47c03cfcf3d38
Green Camper <stefan.palacios@gmail.com>
2023-04-22 16:26:16 -0500

    Added boilerplate server.js code, then added columns for catagory table

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
-
+//creating the Category model
 class Category extends Model {}
-
+//creation of the fields and columns of the Catagory model
 Category.init(
   {
     // define columns
+    //id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    //product column
+    product: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
