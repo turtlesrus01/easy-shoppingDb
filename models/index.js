acf05618ea7bd89312de1aa569c08dad75928370
Green Camper <stefan.palacios@gmail.com>
2023-04-22 20:51:15 -0500

    Added Sequelize associations to index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  //an alias for the retrieved data
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  //an alias for the retrieved data
+  as: 'tagged_products'
+});
 
 module.exports = {
   Product,
